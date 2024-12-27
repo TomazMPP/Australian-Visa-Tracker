f69729b75f5ff2215966f4bfdde0bb269e4db629
Tomaz Pontes <tomazmppontes@gmail.com>
2024-12-27 03:38:48 -0300

    feat: header navigation back home

--- frontend/src/components/ui/header.jsx
+++ frontend/src/components/ui/header.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/`);
+  };
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
@@ -12,12 +18,12 @@ const Header = () => {
        <header className="fixed top-0 left-0 w-full backdrop-blur-sm ">
   <div className="container mx-auto flex justify-between items-center py-3 px-4 max-w-5xl">
         <div>
-          <span role="img" aria-label="logo" className="text-2xl">
+          <span role="img" aria-label="logo" className="text-2xl cursor-pointer" onClick={handleClick}>
           🦘
           </span>
         </div>
         <div>
-          <h1 className="text-xl font-semibold dark:text-white">Australian Visa Tracker</h1>
+          <h1 className="text-xl font-semibold dark:text-white cursor-pointer" onClick={handleClick}>Australian Visa Tracker</h1>
         </div>
         <div>
           <button
