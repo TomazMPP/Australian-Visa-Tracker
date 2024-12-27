ef226a4f42d9b252f1d527ce4072603e67ca3c04
Tomaz Pontes <tomazmppontes@gmail.com>
2024-12-27 03:39:16 -0300

    style: fix text

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -10,13 +10,14 @@ const categoryController = {
       res.status(500).json({ message: 'Internal Server Error'});
     }
   },
+
   getVisasByCategory: async (req, res) => {
     try {
       const { id } = req.params;
       const categories = await categoryModel.getVisasByCategory(id);
       
       if (!categories) {
-        return res.status(404).json({ message: 'Categorie Not Found'});
+        return res.status(404).json({ message: 'Category Not Found'});
       }
 
       res.status(200).json(categories)
